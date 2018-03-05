4fb7ccdfa2c4a3210316b3c90e0ecac01056232b
wang xiang <sdwxiang@hotmail.com>
2018-03-05 18:09:10 +0800

    add app presenter util class

---
+++ src/main/java/work/wangxiang/android/common/App.java
@@ -0,0 +1,24 @@
+package work.wangxiang.android.common;
+
+import android.app.Application;
+
+/**
+ * custom application class
+ * 参考：
+ * https://nfrolov.wordpress.com/2014/07/12/android-using-context-statically-and-in-singletons/
+ * Created by wangxiang on 2018/2/27.
+ */
+
+public class App extends Application {
+    private static App INSTANCE;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        INSTANCE = this;
+    }
+
+    public App getCtx() {
+        return INSTANCE;
+    }
+}
---
+++ src/main/java/work/wangxiang/android/common/PresenterBase.java
@@ -0,0 +1,31 @@
+package work.wangxiang.android.common;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * base presenter
+ * 1. 持有 model 和 view 接口的实例
+ * 2. 管理 RxJava 对象的集合
+ *
+ * Created by wangxiang on 2018/3/3.
+ */
+
+public class PresenterBase<M, V> {
+    protected M model;
+    protected V view;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    void setMV(M model, V view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    protected void addDisposable(Disposable disposable) {
+        compositeDisposable.add(disposable);
+    }
+
+    void stop() {
+        compositeDisposable.clear();
+    }
+}
